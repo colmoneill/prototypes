c63c9f8c5acbff9266f295255fb318d23a176928
colm <mail@colm.be>
2016-04-01 01:19:23 +0200

    datetime data in the idea of not letting sessions last then 1 day

--- scrollometer/script.js
+++ scrollometer/script.js
@@ -9,6 +9,14 @@ document.addEventListener("DOMContentLoaded", function(){
       cumulativeDist = 0,
       prevCumulativeDist = localStorage.prevCumulativeDist;
 
+  var currentdate = new Date();
+  var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+
   function pxToMeters(distance){
     return (distance * 0.02645833333333);
   }
@@ -23,7 +31,6 @@ document.addEventListener("DOMContentLoaded", function(){
   div.setAttribute("style", "font-family: monospace; position: fixed; top: 10; width: 300px; right: 10; background-color: lightblue; padding: 20px; border-radius: 5px;")
   div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + prevCumulativeDist + " px or " + pxToMeters(prevCumulativeDist) + " cm";
 
-
   var v = window.addEventListener("scroll", function(){
     var currentPos = window.scrollY;
     var delta = (currentPos - previousPos);
@@ -32,7 +39,6 @@ document.addEventListener("DOMContentLoaded", function(){
     div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + cumulativeDist + " px or " + pxToMeters(cumulativeDist) + " cm";
     previousPos = currentPos;
     prevCumulativeDist = cumulativeDist;
-    console.log(cumulativeDist, prevCumulativeDist);
-    localStorage.prevCumulativeDist = prevCumulativeDist;
+    localStorage.prevCumulativeDist = [prevCumulativeDist , datetime ];
   })
 })
