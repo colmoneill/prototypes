740d7f41a1e544655d430b716229a63745533bc2
colm <mail@colm.be>
2016-04-01 20:32:05 +0200

    now we check if a previous value was stored for the visited domain, if yes, we take it again, if not, we start from the beginning again. Also, printing out the domain name to the bubble to make clear that this is domain specific

--- scrollometer/script.js
+++ scrollometer/script.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function(){
       previousPos = 0,
       cumulativeDist = 0,
       prevCumulativeDist = 0,
-      prevCumulativeDist = parseInt(localStorage.prevCumulativeDist),
       currentdate = new Date(),
       datetime = "Last entry: " + currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/"
@@ -16,7 +15,16 @@ document.addEventListener("DOMContentLoaded", function(){
                 + currentdate.getHours() + ":"
                 + currentdate.getMinutes() + ":"
                 + currentdate.getSeconds();
-  console.log("found previous CumulativeDist: " + prevCumulativeDist )
+
+  if (localStorage.prevCumulativeDist){
+    var prevCumulativeDist = parseInt(localStorage.prevCumulativeDist, 10);
+    console.log("previous cumulative distance found, using it");
+  }
+  else {
+    var prevCumulativeDist = 0;
+    console.log("no previous cumulative distance found, starting from 0");
+  }
+
   function pxToMeters(distance){
     return (distance * 0.02645833333333);
   }
@@ -26,18 +34,18 @@ document.addEventListener("DOMContentLoaded", function(){
   div.className = "bubble"
   console.log("created bubble div")
   div.setAttribute("style", "font-family: monospace; position: fixed; top: 10px !important; width: 300px; right: 10px !important; background-color: lightblue; padding: 20px; border-radius: 5px;")
-  div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + prevCumulativeDist + " px or " + pxToMeters(prevCumulativeDist) + " cm";
+  div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + prevCumulativeDist + " px or " + pxToMeters(prevCumulativeDist) + " cm <br> on " + window.location.host;
 
   var v = window.addEventListener("scroll", function(){
-    console.log("prevCumulativeDist at start of event listener " + prevCumulativeDist);
+    //console.log("prevCumulativeDist at start of event listener " + prevCumulativeDist);
     var currentPos = window.scrollY;
     var delta = (currentPos - previousPos);
-    cumulativeDist = parseInt(prevCumulativeDist);
+    cumulativeDist = prevCumulativeDist;
     cumulativeDist = cumulativeDist + Math.abs(delta);
-    div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + cumulativeDist + " px or " + pxToMeters(cumulativeDist) + " cm";
+    div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + cumulativeDist + " px or " + pxToMeters(cumulativeDist) + " cm <br> on <code>" + window.location.host + "</code>";
     previousPos = currentPos;
     prevCumulativeDist = cumulativeDist;
-    localStorage.prevCumulativeDist = [parseInt(prevCumulativeDist) , datetime ];
-    console.log("prevCumulativeDist at END of event listener " + prevCumulativeDist);
+    localStorage.prevCumulativeDist = [parseInt(prevCumulativeDist, 10) , datetime ];
+    //console.log("prevCumulativeDist at END of event listener " + prevCumulativeDist);
   })
 })
