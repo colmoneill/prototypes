5922f1e584b0791a2776c1dd21e4db5bb61d6ec8
colm <mail@colm.be>
2016-04-01 13:01:48 +0200

    last changes to scroll meter

--- scrollometer/script.js
+++ scrollometer/script.js
@@ -7,31 +7,29 @@ document.addEventListener("DOMContentLoaded", function(){
       devicePixelRatio = window.devicePixelRatio,
       previousPos = 0,
       cumulativeDist = 0,
-      prevCumulativeDist = localStorage.prevCumulativeDist;
-
-  var currentdate = new Date();
-  var datetime = "Last Sync: " + currentdate.getDate() + "/"
+      prevCumulativeDist = 0,
+      prevCumulativeDist = parseInt(localStorage.prevCumulativeDist),
+      currentdate = new Date(),
+      datetime = "Last entry: " + currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/"
                 + currentdate.getFullYear() + " @ "
                 + currentdate.getHours() + ":"
                 + currentdate.getMinutes() + ":"
                 + currentdate.getSeconds();
-
+  console.log("found previous CumulativeDist: " + prevCumulativeDist )
   function pxToMeters(distance){
     return (distance * 0.02645833333333);
   }
 
-  //document.head.insertAdjacentHTML( 'beforeend', '<link rel=stylesheet href="http://pzwart1.wdka.hro.nl/~colm/prototypes/scrollometer/bubble-styles.css">' );
-  ///home/colm/ownCloud/PZI/Litteracy-as-a-dependency/prototypes/scrollometer/bubble-styles.css
-
   var div = document.createElement("div")
   document.body.appendChild(div)
   div.className = "bubble"
-  div.style.position = "fixed";
-  div.setAttribute("style", "font-family: monospace; position: fixed; top: 10; width: 300px; right: 10; background-color: lightblue; padding: 20px; border-radius: 5px;")
+  console.log("created bubble div")
+  div.setAttribute("style", "font-family: monospace; position: fixed; top: 10px !important; width: 300px; right: 10px !important; background-color: lightblue; padding: 20px; border-radius: 5px;")
   div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + prevCumulativeDist + " px or " + pxToMeters(prevCumulativeDist) + " cm";
 
   var v = window.addEventListener("scroll", function(){
+    console.log("prevCumulativeDist at start of event listener " + prevCumulativeDist);
     var currentPos = window.scrollY;
     var delta = (currentPos - previousPos);
     cumulativeDist = parseInt(prevCumulativeDist);
@@ -39,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function(){
     div.innerHTML = "This page offers <code>" + fullHeight + " px</code> of potential or " + pxToMeters(fullHeight)*devicePixelRatio  + " cm <br><br> Your device pixel ratio is " + devicePixelRatio + " <br><br> and You've scrolled " + cumulativeDist + " px or " + pxToMeters(cumulativeDist) + " cm";
     previousPos = currentPos;
     prevCumulativeDist = cumulativeDist;
-    localStorage.prevCumulativeDist = [prevCumulativeDist , datetime ];
+    localStorage.prevCumulativeDist = [parseInt(prevCumulativeDist) , datetime ];
+    console.log("prevCumulativeDist at END of event listener " + prevCumulativeDist);
   })
 })
