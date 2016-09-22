c38c63bc140c4e03dfdd689c5061c1ad66e00b31
Colm <mail@colm.be>
2016-09-22 22:02:51 +0200

    forgot a dash before the flag for ng

--- cross-publish/__crosspublish.py
+++ cross-publish/__crosspublish.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description='Cross publish your texts to Mediaw
 parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='Your input text file', required=True)
 parser.add_argument('-wp', '--wikipage', help='Name of the wiki page to post to. If this page does not exist it will be created. FYI, all of these pages get posted to your namespace on the mdwiki.', required=True)
 parser.add_argument('-m', '--message', help='Your edit summary and commit message, use quotes for strings. If not provided an automatic message is placed with a timestamp.')
-parser.add_argument('ng', '--nogit', action='store_true', help='Use this flag if youre not pushing to a git repository.')
+parser.add_argument('-ng', '--nogit', action='store_true', help='Use this flag if youre not pushing to a git repository.')
 args = parser.parse_args()
 # publish to mediawiki
 input = args.file.name
