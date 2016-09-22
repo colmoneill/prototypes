72e2d33927ce23eb4d6a2d1b04b55a70828d3e3f
Colm <mail@colm.be>
2016-09-22 14:25:26 +0200

    progress, although the last while has been spent fixing a broken git from the scripts :)

--- cross-publish/__crosspublish.py
+++ cross-publish/__crosspublish.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python
 
-# settings for user:
-# wikiusername =
-# wikipassword =
-
 import argparse
 import mwclient
 import pypandoc
+import os
+from git import *
+import datetime
+join = os.path.join
+
+# settings for user:
+# wikiusername =
+# wikipassword =
+# repo =
+repopath = '/home/colm/git/prototypes/'
+repo = Repo('/home/colm/git/prototypes/')
+remoteURL = 'git@github.com:colmoneill/prototypes.git'
 
 parser = argparse.ArgumentParser(description='Cross publish your texts to Mediawikis and git repos')
 parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='your input text file', required=True)
 parser.add_argument('-wp', '--wikipage', help='name of the wiki page to post to. If this page does not exist it will be created.', required=True)
 args = parser.parse_args()
 
+# publish to mediawiki
 input = args.file.name
 output = pypandoc.convert_file(input, 'mediawiki')
+now = datetime.datetime.now
 site = mwclient.Site('pzwiki.wdka.nl', '/mw-mediadesign/')
 site.force_login = True
 site.login(wikiusername, wikipassword)
 wikipagename = args.wikipage
-page = site.pages['User:' + username + '/'+ wikipagename]
+page = site.pages['User:' + wikiusername + '/'+ wikipagename]
 page.save(output, 'version pushed from crosspublish.py')
-print 'Converted ' + input + ' to mediawiki syntax and published on ' + 'https://pzwiki.wdka.nl/mw-mediadesign/' 'User:' + username + '/'+ wikipagename
+print 'Converted ' + input + ' to mediawiki syntax and published on ' + 'https://pzwiki.wdka.nl/mw-mediadesign/' 'User:' + wikiusername + '/'+ wikipagename
+
+#publish to a git repo
+git = repo.git
+repo.config_reader()
+cw = repo.config_writer()
+index = repo.index
+git.pull
+git.add([input])
+print 'added input to git'
+index.commit("Crosspublish from script ").type == 'commit'
+print 'committed automatic commit message with datetime'
+git.push()
