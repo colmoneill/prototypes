39521c4da16d9c04cc54f82093e02d5738f8ef57
Colm <mail@colm.be>
2016-09-22 21:58:57 +0200

    custom commit message on command line (optional) and nogit flag in case of no git pushing

--- cross-publish/__crosspublish.py
+++ cross-publish/__crosspublish.py
@@ -17,8 +17,9 @@ join = os.path.join
 parser = argparse.ArgumentParser(description='Cross publish your texts to Mediawikis and git repos')
 parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='your input text file', required=True)
 parser.add_argument('-wp', '--wikipage', help='name of the wiki page to post to. If this page does not exist it will be created.', required=True)
+parser.add_argument('-m', '--message', help='your edit summary and commit message, use quotes for strings')
+parser.add_argument('ng', '--nogit', action='store_true', help='use this flag if youre not pushing to a git repo')
 args = parser.parse_args()
-
 # publish to mediawiki
 input = args.file.name
 output = pypandoc.convert_file(input, 'mediawiki')
@@ -31,17 +32,26 @@ page = site.pages['User:' + wikiusername + '/'+ wikipagename]
 timenow = datetime.now()
 now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 automatic_edit_summary = 'version pushed from crosspublish.py at ' + now
-print (automatic_edit_summary)
-page.save(output, automatic_edit_summary)
+if args.message:
+    page.save(output, args.message)
+else:
+    page.save(output, automatic_edit_summary)
 print('Converted ' + input + ' to mediawiki syntax and published on ' + 'https://pzwiki.wdka.nl/mw-mediadesign/' 'User:' + wikiusername + '/'+ wikipagename)
 
 #publish to a git repo
-gitrepo = gitpath.root()
-repo = Repo(gitrepo)
-index = repo.index
-gitinput = os.path.abspath(input)
-index.add([gitinput])
-index.commit(automatic_edit_summary)
-git = repo.git
-git.push()
-print("Pushed " + input + "to " + gitrepo )
+nogit = args.nogit
+if nogit is True:
+    print('git option set to flase, finished cross publishing')
+else:
+    gitrepo = gitpath.root()
+    repo = Repo(gitrepo)
+    index = repo.index
+    gitinput = os.path.abspath(input)
+    index.add([gitinput])
+    if args.message:
+        index.commit(args.message)
+    else:
+        index.commit(automatic_edit_summary)
+        git = repo.git
+        git.push()
+    print("Pushed " + input + " to " + repo.remotes.origin.url )
