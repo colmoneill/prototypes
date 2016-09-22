48e659c072fdd17aee0181b6a5a0b33bbbcbb986
Colm <mail@colm.be>
2016-09-22 20:39:10 +0200

    Multiple issues to keep command simple:
    to be able to call the utility system wide (avoiding having to copy
    the script in each and every location you would use it from) I had
    to find the root of the git project that the input was set from.
    
    Finding the .git folder (reversing backwards recursively) is beyond
    my abilities, so I found a lib called gitpath that does exactly
    this for me.
    
    Gitpath (https://github.com/maxnoe/python-gitpath) is written in
    Python3 however so I had to undergo a bunch of translations I had
    no idea about, and install Pip3 etc etc.
    
    Before all that I did go back to GitPython a bit, it has a utility
    to find git roots, but it's only part of an index function that
    depends on the main Repo object, that needs to be set manually.
    I asked about this on SO:
    http://stackoverflow.com/questions/39645916/finding-git-root-from-file-with-gitpython
    
    Python3 conversion done anyway, and now it all works, I think.

--- cross-publish/__crosspublish.py
+++ cross-publish/__crosspublish.py
@@ -5,14 +5,13 @@ import mwclient
 import pypandoc
 import os
 from git import *
+import gitpath # pip install git+https://github.com/maxnoe/python-gitpath
 import datetime
 join = os.path.join
 
 # settings for user:
 # wikiusername = your /mediadesign mw username
 # wikipassword = your pw
-# gitrepo = example: '/home/colm/git/prototypes' -> the location of your .git folder
-
 
 parser = argparse.ArgumentParser(description='Cross publish your texts to Mediawikis and git repos')
 parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='your input text file', required=True)
@@ -29,13 +28,22 @@ site.login(wikiusername, wikipassword)
 wikipagename = args.wikipage
 page = site.pages['User:' + wikiusername + '/'+ wikipagename]
 page.save(output, 'version pushed from crosspublish.py')
-print 'Converted ' + input + ' to mediawiki syntax and published on ' + 'https://pzwiki.wdka.nl/mw-mediadesign/' 'User:' + wikiusername + '/'+ wikipagename
+print('Converted ' + input + ' to mediawiki syntax and published on ' + 'https://pzwiki.wdka.nl/mw-mediadesign/' 'User:' + wikiusername + '/'+ wikipagename)
 
 #publish to a git repo
+#print(os.path.abspath(input))
+gitrepo = gitpath.root()
+print('found git root based on input')
+#set the found path to the GitPython main Repo object
 repo = Repo(gitrepo)
-git = repo.git
+# use GitPython index functions for safe interactions
 index = repo.index
 gitinput = os.path.abspath(input)
+#print (gitinput)
+#add input file to git
 index.add([gitinput])
+#commit message
 index.commit("is this script working?")
+git = repo.git
+#push
 git.push()
