4d0df1fd9b34aaf0919c3ae1c274a01d66577124
Colm <mail@colm.be>
2016-10-31 00:26:27 +0100

    testing stopwords, not sure they are going to be useful for my case here

--- modes-of-address/readfile.py
+++ modes-of-address/readfile.py
@@ -1,19 +1,34 @@
 import re
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
 file = open("en_GB.po", "r")
+stop_words = set(stopwords.words("english"))
 
 ## not sure about the approach here; do I parse the content of the .po
 ## file myself or do I feed it straight into NLTK?
 ## option 1
 content = file.read()
 #print content
-match = re.findall(r"msgstr\s\"([^\"\\]*)\"", content)
-match = " ".join(match)
-match = match.decode('utf8')
-print match
+#match = re.findall(r"msgstr\s\"([^\"\\]*)\"", content)
+#match = " ".join(match)
+#match = match.decode('utf8')
+#print match
 #print(sent_tokenize(match))
 
 ## option 2
 content = content.decode('utf8')
 #print(sent_tokenize(match))
 #print(sent_tokenize(content))
+
+content = word_tokenize(content)
+
+print (content)
+
+#filtered_content = []
+#
+#for word in content:
+#    if word not in stop_words:
+#        filtered_content.append(word)
+#
+#print (filtered_content)
