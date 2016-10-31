6a1d179161464efc5bb8ee18b389aa36148bac02
Colm <mail@colm.be>
2016-10-31 14:05:02 +0100

    comparison result page and a bit too fast a readme

---
+++ modes-of-address/simple_part_of_speech_vim.py
@@ -0,0 +1,32 @@
+import re
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize, PunktSentenceTokenizer
+from operator import itemgetter
+from collections import Counter
+
+file = open("vimtutor.txt", "r")
+output = open('vimtutor_tagged_language.txt', 'w')
+output2 = open('vimtutor_occurences_of_language.txt', 'w')
+## not sure about the approach here; do I parse the content of the .po
+## file myself or do I feed it straight into NLTK?
+## option 1
+content = file.read()
+#print content
+#match = re.findall(r"msgstr\s\"([^\"\\]*)\"", content)
+#match = " ".join(match)
+#match = match.decode('utf8')
+
+text = word_tokenize(content)
+processed = nltk.pos_tag(text)
+sorted_processed = sorted(processed,key=itemgetter(1))
+print (sorted_processed)
+#for item in sorted_processed:
+#    print>>output, item
+
+counter = (Counter(sorted_processed))
+#for item in counter.items():
+#    print>>output2, item
+
+count_pos = [x[1] for x in sorted_processed]
+count_pos = (Counter(count_pos))
+print (count_pos)
